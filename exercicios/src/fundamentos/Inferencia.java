c8e95b8ece445ca20ffad703d02b2ce16655537b
Guilherme Lopes <guilhermxlopes@gmail.com>
2022-10-27 08:53:37 -0300

    feat: inferências

---
+++ exercicios/src/fundamentos/Inferencia.java
@@ -0,0 +1,12 @@
+package fundamentos;
+
+public class Inferencia {
+    public static void main(String[] args) {
+
+        double a = 4.5;
+        System.out.println(a);
+
+        var b = "Texto"; // Java faz inferência do tipo da variável
+        System.out.println(b);
+    }
+}
